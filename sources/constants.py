3318afc49a9066e59bc1111ea0f6deb7d9fba3b1
J-Rios <jose_23991@hotmail.com>
2017-08-23 18:41:19 +0200

    Development branch creation & skeleton sources added

---
+++ sources/constants.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+'''
+Script:
+    constants.py
+Description:
+    Constants values for myrssbot.py.
+Author:
+    Jose Rios Rubio
+Creation date:
+    23/08/2017
+Last modified date:
+    23/08/2017
+Version:
+    0.0.0
+'''
+
+####################################################################################################
+
+### Constants ###
+CONST = {
+    'Developer' : '@JoseTLG',
+    'Date' : '23/08/2017',
+    'Version' : '0.0.0'
+}
---
+++ sources/myrssbot.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+'''
+Script:
+    myrss.py
+Description:
+    Telegram Bot that let you subscribe and follow customs RSS Feeds.
+Autor:
+    Jose Rios Rubio
+Fecha de creacion:
+    23/08/2017
+Fecha de ultima modificacion:
+    23/08/2017
+Version:
+    0.0.0
+'''
+
+####################################################################################################
+
+### Imported modules ###
+from constants import CONST
+
+####################################################################################################
+
+### Globals ###
+
+
+####################################################################################################
+
+### Functions ###
+
+
+####################################################################################################
+
+### Received commands handlers ###
+
+
+####################################################################################################
+
+### Main function ###
+def main():
+    ''' Main Function'''
+    print("Executed")
+
+
+####################################################################################################
+
+### Execute the main function if the file is not an imported module ###
+if __name__ == '__main__':
+    main()
